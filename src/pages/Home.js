08c64b29362db9b512d707e4494e2f760e5246c2
manjaysinghania <122433251+manjaysinghania@users.noreply.github.com>
2024-04-09 09:50:26 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,21 @@
 import Homeitems from "../component/Homeitems"
-import Spinner from "../component/Spinner";
+
+import items from '../items'
 
 
-import { useEffect,useState } from "react"
+
 
 
 function Home(){
 
-  const [loading,setloading]=useState(false);
-  const[posts,setposts]=useState([])
-
-  async function fetchdata(){
-    setloading(true)
-    try{  
-      const output=await fetch("https://myntra-1.vercel.app/items")
-      const data=await output.json()
-      
-       setposts(data.items[0])
-       
-      }
-    catch(error){
-      console.log('error aa gyaaa')
-      setposts([])
-    }
-    setloading(false)
-  }
-  useEffect(()=>{
-       fetchdata()
-      
-  },[])
+  
   return(<div>
            <main>
               <div className="items-container">
               {
-                 loading?(<Spinner/>):
-                 (posts.length>0)?
-                  (posts.map((item)=>{
+                
+                 (items.length>0)?
+                  (items.map((item)=>{
                     console.log(item)
                     return(<Homeitems key={item.id} item={item}/>)
                   })):(<center>No Data Found</center>)
